1cea52719be47d18be7eceb60501833859e8e126
Micaella Colombo <micaelc@g.clemson.edu>
2022-01-06 20:21:38 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,13 @@
-# == Schema Information
-#
-# Table name: students
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  hometown   :string
-#  birthday   :datetime
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query == ''
+      @students = Student.all
+    else
+      @students = Student.where("name like ?", "%#{query}%")
+    end
+  end
 end
